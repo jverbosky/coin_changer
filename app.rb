bd15354edca3cc44712565e47385d47b0eae7add
jverbosk <jverbosk@gmail.com>
2017-02-17 21:46:08 -0500

    coin changer app working via sinatra

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?  # automatically reload app.rb on save via sinatra-contrib gem
+require_relative 'coin_changer.rb'
+
+get '/' do
+  erb :get_amount
+end
+
+post '/your_change' do
+  amount = params[:amount].to_i
+  change = make_change(amount)
+  "#{change}"
+end
