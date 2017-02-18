e1f91eeaf3568c5c68072941a9f942ef61f98f2c
jverbosk <jverbosk@gmail.com>
2017-02-17 20:56:41 -0500

    added logic to only add coin type to coin_count when needed

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,30 +1,24 @@
-coins = {
-  quarters: 25,
-  dimes: 10,
-  nickels: 5,
-  pennies: 1
-}
-
-coin_count = {
-  quarters: 0,
-  dimes: 0,
-  nickels: 0,
-  pennies: 0
-}
-
-amount = 93  # {:quarters=>3, :dimes=>1, :nickels=>1, :pennies=>3}
+coins = { quarters: 25, dimes: 10, nickels: 5, pennies: 1 }
+coin_count = {}
+amount = 8
 
 # def make_change(amount)
 
 coins.each do |type, value|
+  if amount >= value
+    coin_count[type] = 0
+  end
   while amount >= value
-    puts "Coin type (#{type}) and amount (#{value})."
-    puts "Amount: #{amount}"
     amount = amount - value
-    puts "After subtracting: #{amount}"
     coin_count[type] += 1
   end
 end
+
+# coin_count.each do |type, count|
+#   if count == 0
+#     coin_count.delete(type)
+#   end
+# end
 
 # end
 
