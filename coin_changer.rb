10069d4c8b4fc745da19c0eb74c5288b8057ecd7
jverbosk <jverbosk@gmail.com>
2017-02-17 21:03:45 -0500

    rewrote statements as a function, tests all passing

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,32 +1,23 @@
-coins = { quarters: 25, dimes: 10, nickels: 5, pennies: 1 }
-coin_count = {}
-amount = 8
-
-# def make_change(amount)
-
-coins.each do |type, value|
-  if amount >= value
-    coin_count[type] = 0
+def make_change(amount)
+  coins = { quarters: 25, dimes: 10, nickels: 5, pennies: 1 }
+  coin_count = {}
+  coins.each do |type, value|
+    if amount >= value
+      coin_count[type] = 0
+    end
+    while amount >= value
+      amount = amount - value
+      coin_count[type] += 1
+    end
   end
-  while amount >= value
-    amount = amount - value
-    coin_count[type] += 1
-  end
+  return coin_count
 end
 
-# coin_count.each do |type, count|
-#   if count == 0
-#     coin_count.delete(type)
-#   end
-# end
-
-# end
-
-# coins.each do |type, amount|
-#   coin_count[type] += 1
-# end
-
-p coin_count
-# make_change(26)
-# p coin_count
-
+# Sandbox testing
+p make_change(8)
+p make_change(11)
+p make_change(19)
+p make_change(27)
+p make_change(31)
+p make_change(48)
+p make_change(93)
