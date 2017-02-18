903a70d2b5773cbdbb886d617d7eadecfbd6ce73
jverbosk <jverbosk@gmail.com>
2017-02-17 21:18:01 -0500

    refactored make_change method, all tests passing

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,23 +1,35 @@
+# # Original make_change method
+# def make_change(amount)
+#   coins = { quarter: 25, dime: 10, nickel: 5, penny: 1 }
+#   coin_count = {}
+#   coins.each do |type, value|
+#     if amount >= value
+#       coin_count[type] = 0
+#     end
+#     while amount >= value
+#       amount = amount - value
+#       coin_count[type] += 1
+#     end
+#   end
+#   return coin_count
+# end
+
+# Refactored make_change method
 def make_change(amount)
-  coins = { quarters: 25, dimes: 10, nickels: 5, pennies: 1 }
+  coins = { quarter: 25, dime: 10, nickel: 5, penny: 1 }
   coin_count = {}
   coins.each do |type, value|
-    if amount >= value
-      coin_count[type] = 0
-    end
-    while amount >= value
-      amount = amount - value
-      coin_count[type] += 1
-    end
+    coin_count[type] = 0 if amount >= value
+    (amount -= value; coin_count[type] += 1) while amount >= value
   end
   return coin_count
 end
 
 # Sandbox testing
-p make_change(8)
-p make_change(11)
-p make_change(19)
-p make_change(27)
-p make_change(31)
-p make_change(48)
-p make_change(93)
+# p make_change(8)
+# p make_change(11)
+# p make_change(19)
+# p make_change(27)
+# p make_change(31)
+# p make_change(48)
+# p make_change(93)
